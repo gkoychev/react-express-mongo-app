479a9fde3f634ea063ba75031e50ec1ae8314a4f
Georgy Koychev <kgm.pochta@gmail.com>
2020-02-18 12:22:22 +0200

    cleanup client

--- server/src/api/posts.ts
+++ server/src/api/posts.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response, NextFunction } from "express";
 
 import PostService from "../services/PostService";
+import { PAGE_LIMIT } from "../constants";
 
 export default (router: Router) => {
   router.get(
@@ -8,8 +9,9 @@ export default (router: Router) => {
     async (req: Request, res: Response, next: NextFunction) => {
       try {
         const page = req.params.page || 1;
+        const limit = req.query.limit || PAGE_LIMIT;
 
-        const pageData = await PostService.getPage(+page);
+        const pageData = await PostService.getPage(+page, +limit);
 
         res.json(pageData);
       } catch (err) {
